b837b29493bc1e08c3c12a4a6f574084236b89f1
Lei Zongmin <leizongmin@gmail.com>
2012-10-23 11:40:05 +0800

    当系统已存在SQL类时，不创建新的SQL类，用于兼容旧版本的代码

--- leiphp.php
+++ leiphp.php
@@ -9,8 +9,8 @@
 // 开始时间
 define('APP_TIMESTAMP_START', microtime(true));
 
-// 处理不同的请求方法
-function _leiapp_request_method_router () {
+/* 处理不同的请求方法 */
+function _leiphp_request_method_router () {
   $method = strtolower($_SERVER['REQUEST_METHOD']);
   $funcname = "method_$method";
   define('APP_TIMESTAMP_ROUTE', microtime(true));
@@ -18,7 +18,7 @@ function _leiapp_request_method_router () {
     $funcname();
   } elseif (function_exists('method_all')) {
     $funcname = 'method_all';
-    METHOD_ALL();
+    method_all();
   } else {
     $funcname = 'method_undefine';
   }
@@ -40,249 +40,254 @@ function _leiapp_request_method_router () {
 </div>";
   }
 }
-register_shutdown_function('_leiapp_request_method_router');
+register_shutdown_function('_leiphp_request_method_router');
 
 
 /**
  * MySQL 数据库操作
  */
-class SQL {
-  /**
-   * 连接到数据库
-   * 成功返回true, 失败返回false
-   *
-   * @param {string} $server
-   * @param {string} $username
-   * @param {string} $password
-   * @param {string} $database
-   * @return {bool}
-   */
-  public static function connect ($server = 'localhost:3306', $username = 'root', $password = '', $database = '') {
-    $timestamp = microtime(true);
-    mysql_connect($server, $username, $password);
-    $r = mysql_select_db($database);
-    DEBUG::put('Connected: '.$username.'@'.$server.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'MySQL');
-    // 设置默认字符集为utf-8
-    // SQL::update('set names utf8');
-    return $r;
-  }
-  
-  /**
-   * 获取出错信息
-   * 返回数据格式：  {id:出错ID, error:出错描述}
-   *
-   * @return {array}
-   */
-  public static function error () {
-    return array(
-      'id'  =>  mysql_errno(),
-      'error' =>  mysql_error()
-      );
-  }
-
-  /**
-   * 返回出错代码
-   *
-   * @return {int}
-   */
-  public static function errno () {
-    return mysql_errno();
-  }
-
-  /**
-   * 返回出错描述信息
-   *
-   * @return {string}
-   */
-  public static function errmsg () {
-    return mysql_error();
-  }
-  
-  /**
-   * 查询并返回所有数据
-   * 格式为： [{字段名:值, 字段名:值 ...}, ...]，返回FALSE表示失败
-   *
-   * @param {string} $sql
-   * @return {array}
-   */
-  public static function getAll ($sql) {
-    $timestamp = microtime(true);
-    $r = mysql_query($sql);
-    if (mysql_errno()) {
-      DEBUG::put('Query: '.$sql.' fail: #'.mysql_errno(), 'MySQL');
-      return FALSE;
+if (!class_exists('SQL', FALSE)) {
+  class SQL {
+    /**
+     * 连接到数据库
+     * 成功返回true, 失败返回false
+     *
+     * @param {string} $server
+     * @param {string} $username
+     * @param {string} $password
+     * @param {string} $database
+     * @return {bool}
+     */
+    public static function connect ($server = 'localhost:3306', $username = 'root', $password = '', $database = '') {
+      $timestamp = microtime(true);
+      mysql_connect($server, $username, $password);
+      $r = mysql_select_db($database);
+      DEBUG::put('Connected: '.$username.'@'.$server.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'MySQL');
+      // 设置默认字符集为utf-8
+      // SQL::update('set names utf8');
+      return $r;
     }
-    $data = array();
-    while ($row = mysql_fetch_array($r, MYSQL_ASSOC)) {
-      $data[] = $row;
+
+    /**
+     * 获取出错信息
+     * 返回数据格式：  {id:出错ID, error:出错描述}
+     *
+     * @return {array}
+     */
+    public static function error () {
+      return array(
+        'id'  =>  mysql_errno(),
+        'error' =>  mysql_error()
+        );
     }
-    DEBUG::put('Query: '.$sql.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'MySQL');
-    return count($data) < 1 ? FALSE : $data;
-  }
-  public static function getData ($sql) {
-    return SQL::getAll($sql);
-  }
-  
-  /**
-   * 查询并返回一行数据 格式为 {字段名:值, 字段名:值 ...}，返回FALSE表示失败
-   *
-   * @param {string} $sql
-   * @return {array}
-   */
-  public static function getOne ($sql) {
-    $sql .= ' LIMIT 1';
-    $data = SQL::getAll($sql);
-    return $data == FALSE ? FALSE : $data[0];
-  }
-  public static function getLine ($sql) {
-    return SQL::getOne($sql);
-  }
-  
-  /**
-   * 执行SQL命令 返回影响的记录行数
-   *
-   * @param {string} $sql
-   * @return {int}
-   */
-  public static function update ($sql) {
-    $timestamp = microtime(true);
-    mysql_query($sql);
-    if (mysql_errno()) {
-      DEBUG::put('Query: '.$sql.' fail: #'.mysql_errno(), 'MySQL');
-    } else {
+
+    /**
+     * 返回出错代码
+     *
+     * @return {int}
+     */
+    public static function errno () {
+      return mysql_errno();
+    }
+
+    /**
+     * 返回出错描述信息
+     *
+     * @return {string}
+     */
+    public static function errmsg () {
+      return mysql_error();
+    }
+
+    /**
+     * 查询并返回所有数据
+     * 格式为： [{字段名:值, 字段名:值 ...}, ...]，返回FALSE表示失败
+     *
+     * @param {string} $sql
+     * @return {array}
+     */
+    public static function getAll ($sql) {
+      $timestamp = microtime(true);
+      $r = mysql_query($sql);
+      if (mysql_errno()) {
+        DEBUG::put('Query: '.$sql.' fail: #'.mysql_errno(), 'MySQL');
+        return FALSE;
+      }
+      $data = array();
+      while ($row = mysql_fetch_array($r, MYSQL_ASSOC)) {
+        $data[] = $row;
+      }
       DEBUG::put('Query: '.$sql.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'MySQL');
+      return count($data) < 1 ? FALSE : $data;
     }
-    return mysql_affected_rows();
-  }
-  public static function runSql ($sql) {
-    return SQL::runSql($sql);
-  }
-  
-  /**
-   * 取最后插入ID
-   *
-   * @return {int}
-   */
-  public static function id () {
-    return mysql_insert_id();
-  }
-  public static function lastId () {
-    return SQL::id();
-  }
-  
-  /**
-   * 转换为SQL安全字符串
-   *
-   * @param {string} $str
-   * @return {string}
-   */
-  public static function escape ($str) {
-    return  mysql_escape_string($str);
+    public static function getData ($sql) {
+      return SQL::getAll($sql);
+    }
+
+    /**
+     * 查询并返回一行数据 格式为 {字段名:值, 字段名:值 ...}，返回FALSE表示失败
+     *
+     * @param {string} $sql
+     * @return {array}
+     */
+    public static function getOne ($sql) {
+      $sql .= ' LIMIT 1';
+      $data = SQL::getAll($sql);
+      return $data == FALSE ? FALSE : $data[0];
+    }
+    public static function getLine ($sql) {
+      return SQL::getOne($sql);
+    }
+
+    /**
+     * 执行SQL命令 返回影响的记录行数
+     *
+     * @param {string} $sql
+     * @return {int}
+     */
+    public static function update ($sql) {
+      $timestamp = microtime(true);
+      mysql_query($sql);
+      if (mysql_errno()) {
+        DEBUG::put('Query: '.$sql.' fail: #'.mysql_errno(), 'MySQL');
+      } else {
+        DEBUG::put('Query: '.$sql.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'MySQL');
+      }
+      return mysql_affected_rows();
+    }
+    public static function runSql ($sql) {
+      return SQL::runSql($sql);
+    }
+
+    /**
+     * 取最后插入ID
+     *
+     * @return {int}
+     */
+    public static function id () {
+      return mysql_insert_id();
+    }
+    public static function lastId () {
+      return SQL::id();
+    }
+
+    /**
+     * 转换为SQL安全字符串
+     *
+     * @param {string} $str
+     * @return {string}
+     */
+    public static function escape ($str) {
+      return  mysql_escape_string($str);
+    }
   }
 }
-
 /**
  * 调试信息流操作
  */
-class DEBUG {
-  public static $stack = '';
-  
-  /**
-   * 添加到DEBUG流
-   *
-   * @param {string} $msg
-   * @param {string} $title
-   */
-  public static function put ($msg = '', $title = '') {
-    if (defined('APP_DEBUG')) {
-      if (!empty($title)) {
-        $msg = '['.$title.'] '.$msg;
+if (!class_exists('DEBUG', FALSE)) {
+  class DEBUG {
+    public static $stack = '';
+
+    /**
+     * 添加到DEBUG流
+     *
+     * @param {string} $msg
+     * @param {string} $title
+     */
+    public static function put ($msg = '', $title = '') {
+      if (defined('APP_DEBUG')) {
+        if (!empty($title)) {
+          $msg = '['.$title.'] '.$msg;
+        }
+        DEBUG::$stack .= $msg."\r\n";
       }
-      DEBUG::$stack .= $msg."\r\n";
     }
-  }
-  
-  /**
-   * 获取DEBUG流
-   *
-   * @return {string}
-   */
-  public static function get () {
-    return DEBUG::$stack;
-  }
-  
-  /**
-   * 清空DEBUG流，并返回之前的信息
-   *
-   * @return {string}
-   */
-  public static function clear () {
-    $ret = DEBUG::$stack;
-    DEBUG::$stack = '';
-    return $ret;
+
+    /**
+     * 获取DEBUG流
+     *
+     * @return {string}
+     */
+    public static function get () {
+      return DEBUG::$stack;
+    }
+
+    /**
+     * 清空DEBUG流，并返回之前的信息
+     *
+     * @return {string}
+     */
+    public static function clear () {
+      $ret = DEBUG::$stack;
+      DEBUG::$stack = '';
+      return $ret;
+    }
   }
 }
 
 /**
  * 上传文件管理
  */
-class UPLOAD {
-  /**
-   * 获取上传文件
-   * 返回格式：{name: 名称, type: 文件MIME类型, size: 大小, tmp_name: 临时文件名}
-   *
-   * @param {string} $filename
-   * @return {array}
-   */
-  public static function get ($filename) {
-    if (isset($_FILES[$filename])) {
-      $uploaded_file = array(
-        'name' => $_FILES[$filename]['name'],
-        'type' => $_FILES[$filename]['type'],
-        'size' => $_FILES[$filename]['size'],
-        'tmp_name' => $_FILES[$filename]['tmp_name']
+if (!class_exists('UPLOAD', FALSE)) {
+  class UPLOAD {
+    /**
+     * 获取上传文件
+     * 返回格式：{name: 名称, type: 文件MIME类型, size: 大小, tmp_name: 临时文件名}
+     *
+     * @param {string} $filename
+     * @return {array}
+     */
+    public static function get ($filename) {
+      if (isset($_FILES[$filename])) {
+        $uploaded_file = array(
+          'name' => $_FILES[$filename]['name'],
+          'type' => $_FILES[$filename]['type'],
+          'size' => $_FILES[$filename]['size'],
+          'tmp_name' => $_FILES[$filename]['tmp_name']
+          );
+      } elseif (isset($_POST[$filename])) {
+        $uploaded_file = array(
+          'name' => $_POST[$filename],
+          );
+      } elseif (isset($GLOBALS['HTTP_POST_FILES'][$filename])) {
+        global $HTTP_POST_FILES;
+        $uploaded_file = array(
+          'name' => $HTTP_POST_FILES[$filename]['name'],
+          'type' => $HTTP_POST_FILES[$filename]['type'],
+          'size' => $HTTP_POST_FILES[$filename]['size'],
+          'tmp_name' => $HTTP_POST_FILES[$filename]['tmp_name']
+          );
+      } elseif (isset($GLOBALS['HTTP_POST_VARS'][$filename])) {
+        global $HTTP_POST_VARS;
+        $uploaded_file = array(
+          'name' => $HTTP_POST_VARS[$filename],
+          );
+      } else {
+        $uploaded_file = array(
+          'name' => $GLOBALS[$filename . '_name'],
+          'type' => $GLOBALS[$filename . '_type'],
+          'size' => $GLOBALS[$filename . '_size'],
+          'tmp_name' => $GLOBALS[$filename]
         );
-    } elseif (isset($_POST[$filename])) {
-      $uploaded_file = array(
-        'name' => $_POST[$filename],
-        );
-    } elseif (isset($GLOBALS['HTTP_POST_FILES'][$filename])) {
-      global $HTTP_POST_FILES;
-      $uploaded_file = array(
-        'name' => $HTTP_POST_FILES[$filename]['name'],
-        'type' => $HTTP_POST_FILES[$filename]['type'],
-        'size' => $HTTP_POST_FILES[$filename]['size'],
-        'tmp_name' => $HTTP_POST_FILES[$filename]['tmp_name']
-        );
-    } elseif (isset($GLOBALS['HTTP_POST_VARS'][$filename])) {
-      global $HTTP_POST_VARS;
-      $uploaded_file = array(
-        'name' => $HTTP_POST_VARS[$filename],
-        );
-    } else {
-      $uploaded_file = array(
-        'name' => $GLOBALS[$filename . '_name'],
-        'type' => $GLOBALS[$filename . '_type'],
-        'size' => $GLOBALS[$filename . '_size'],
-        'tmp_name' => $GLOBALS[$filename]
-      );
+      }
+      return $uploaded_file;
     }
-    return $uploaded_file;
-  }
 
-  /**
-   * 移动临时文件
-   *
-   * @param {array} $file
-   * @param {string} $target
-   * @return {string}
-   */
-  public static function move ($file, $target) {
-    $timestamp = microtime(true);
-    $source = is_array($file) ? $file['tmp_name'] : $file;
-    move_uploaded_file($source, $target);
-    DEBUG::put('Move '.$source.' to '.$target.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'Upload');
-    return $target;
+    /**
+     * 移动临时文件
+     *
+     * @param {array} $file
+     * @param {string} $target
+     * @return {string}
+     */
+    public static function move ($file, $target) {
+      $timestamp = microtime(true);
+      $source = is_array($file) ? $file['tmp_name'] : $file;
+      move_uploaded_file($source, $target);
+      DEBUG::put('Move '.$source.' to '.$target.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'Upload');
+      return $target;
+    }
   }
 }
 
