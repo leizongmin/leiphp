229ab4473046da2e7ccb568176b544340ebf8812
Zongmin Lei <leizongmin@gmail.com>
2018-05-03 00:57:25 +0800

    v0.4

--- leiphp.php
+++ leiphp.php
@@ -3,7 +3,8 @@
  * LeiPHP
  *
  * @author 老雷<leizongmin@gmail.com>
- * @version 0.3
+ * @version 0.4
+ * @date 2018-05-03 00:53:38
  */
 
 /* 处理不同的请求方法 */
@@ -37,7 +38,7 @@ function _leiphp_request_method_router () {
       font-size: 14px;
       line-height: 1.6em;
       text-align: left;
-      color: #000;
+      color: #02638e;
       padding: 12px 8px;
       border: 1px solid #DDD;
       font-family: \"Microsoft yahei\", \"Helvetica Neue\", \"Lucida Grande\", \"Lucida Sans Unicode\", Helvetica, Arial, sans-serif !important;
@@ -45,6 +46,7 @@ function _leiphp_request_method_router () {
       margin-top: 50px;
 '>Debug<br>Function $funcname spent: {$spent2}ms<br>Total spent: {$spent}ms<br>
 <hr><pre style='
+      color: #02638e;
       font-family: \"Microsoft yahei\", \"Helvetica Neue\", \"Lucida Grande\", \"Lucida Sans Unicode\", Helvetica, Arial, sans-serif !important;
 '>$debug</pre>
 </div>";
@@ -571,315 +573,326 @@ if (!class_exists('ROUTER', false)) {
   DEBUG::put('Class ROUTER is already exists!', 'Warning');
 }
 
-class APP {
+if (!class_exists('TEMPLATE', false)) {
+  class TEMPLATE {
 
-  // 版本号
-  public static $version = 1;
+    // 模板变量
+    public static $locals = array();
 
-  // 是否提前退出
-  public static $is_exit = false;
-
-  // 是否为调试状态
-  public static $is_debug = false;
-
-  // 模板变量
-  public static $locals = array();
-
-  /**
-   * 账户验证加密解密函数 （来自Discuz!的authcode函数）
-   *
-   * @param string $string 明文 或 密文
-   * @param string $operation DECODE表示解密,其它表示加密
-   * @param string $key 密匙
-   * @param int $expiry 密文有效期
-   *
-   * @return string
-   */
-  public static function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
-    $ckey_length = 4;
-    $key = md5($key ? $key : 'leiphp-default-key');
-    $keya = md5(substr($key, 0, 16));
-    $keyb = md5(substr($key, 16, 16));
-    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
-    $cryptkey = $keya.md5($keya.$keyc);
-    $key_length = strlen($cryptkey);
-    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
-    $string_length = strlen($string);
-    $result = '';
-    $box = range(0, 255);
-    $rndkey = array();
-    for($i = 0; $i <= 255; $i++) {
-      $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    /**
+     * 载入模板
+     * 如果指定了参数$layout，则会嵌套一个layout模板
+     *
+     * @param string $name   模板名
+     * @param array $locals  变量
+     * @return string
+     */
+    public static function get ($name, $locals = array()) {
+      if (!pathinfo($name, PATHINFO_EXTENSION)) $name = $name.'.html';
+      $filename = APP_TEMPLATE_ROOT.$name;
+      $timestamp = microtime(true);
+      ob_start();
+      extract($locals, EXTR_SKIP);
+      include($filename);
+      $html = ob_get_clean();
+      DEBUG::put('Render '.$filename.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'Template');
+      return $html;
     }
-    for($j = $i = 0; $i < 256; $i++) {
-      $j = ($j + $box[$i] + $rndkey[$i]) % 256;
-      $tmp = $box[$i];
-      $box[$i] = $box[$j];
-      $box[$j] = $tmp;
-    }
-    for($a = $j = $i = 0; $i < $string_length; $i++) {
-      $a = ($a + 1) % 256;
-      $j = ($j + $box[$a]) % 256;
-      $tmp = $box[$a];
-      $box[$a] = $box[$j];
-      $box[$j] = $tmp;
-      $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
-    }
-    if($operation == 'DECODE') {
-      if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
-        return substr($result, 26);
-      } else {
-        return '';
+
+    /**
+     * 渲染模板，自动使用TEMPLATE::$locals中的数据
+     * 如果指定了参数$layout，则会嵌套一个layout模板
+     *
+     * Examples:
+     * APP::render('template');
+     * APP::render('template', $locals);
+     * APP::render('template', 'layout');
+     * APP::render('template', $locals, 'layout');
+     *
+     * @param string $name
+     * @param array $locals
+     * @param string $layout
+     */
+    public static function render ($name, $locals = array(), $layout = '') {
+      if (!is_array($locals)) {
+        $layout = $locals;
+        $locals = array();
       }
-    } else {
-      return $keyc.str_replace('=', '', base64_encode($result));
-    }
-  }
 
-  /**
-   * 加密账户验证信息
-   *
-   * @param string $string  要加密的字符串
-   * @param string $key     密匙
-   * @param int $expiry     从现在起的有效时间（秒）
-   * @return string
-   */
-  public static function authEncode ($string, $key, $expiry = 0) {
-    return APP::authcode($string, 'ENCODE', $key, $expiry);
-  }
+      foreach (TEMPLATE::$locals as $i => $v) {
+        if (!isset($locals[$i])) $locals[$i] = $v;
+      }
 
-  /**
-   * 解密账户验证信息
-   *
-   * @param string $string  密文
-   * @param string $key     密匙
-   * @return string
-   */
-  public static function authDecode ($string, $key) {
-    return APP::authcode($string, 'DECODE', $key);
-  }
-
-  /**
-   * 加密密码
-   *
-   * @param string $password
-   * @return string
-   */
-  public static function encryptPassword ($password) {
-    $random = strtoupper(md5(rand().rand()));
-    $left = substr($random, 0, 2);
-    $right = substr($random, -2);
-    $newpassword = strtoupper(md5($left.$password.$right));
-    return $left.':'.$newpassword.':'.$right;
-  }
-
-  /**
-   * 验证密码
-   *
-   * @param string $password 待验证的密码
-   * @param string $encrypted 密码加密字符串
-   * @return bool
-   */
-  public static function validatePassword ($password, $encrypted) {
-    $random = explode(':', strtoupper($encrypted));
-    if (count($random) < 3) return false;
-    $left = $random[0];
-    $right = $random[2];
-    $main = $random[1];
-    $newpassword = strtoupper(md5($left.$password.$right));
-    return $newpassword == $main;
-  }
-
-  /**
-   * 显示出错信息
-   *
-   * @param string $msg
-   */
-  public static function showError ($msg) {
-    $accept_type = strtolower(trim($_SERVER['HTTP_ACCEPT']));
-    if (strpos($accept_type, 'json') !== false) {
-      APP::sendJSON(array('error' => $msg));
-    } else {
-      echo "<div style='color: #900;
-            font-size: 16px;
-            border: 1px solid #900;
-            padding: 8px 12px;
-            border-radius: 5px;
-            margin: 12px 0px;'>$msg</div>";
-    }
-  }
-
-  /**
-   * 载入模板
-   * 如果指定了参数$layout，则会嵌套一个layout模板
-   *
-   * @param string $name   模板名
-   * @param array $locals  变量
-   * @return string
-   */
-  public static function getTemplate ($name, $locals = array()) {
-    if (!pathinfo($name, PATHINFO_EXTENSION)) $name = $name.'.html';
-    $filename = APP_TEMPLATE_ROOT.$name;
-    $timestamp = microtime(true);
-    ob_start();
-    extract($locals, EXTR_SKIP);
-    include($filename);
-    $html = ob_get_clean();
-    DEBUG::put('Render '.$filename.' spent: '.round((microtime(true) - $timestamp) * 1000, 3).'ms', 'Template');
-    return $html;
-  }
-
-  /**
-   * 渲染模板，自动使用APP::$locals中的数据
-   * 如果指定了参数$layout，则会嵌套一个layout模板
-   *
-   * Examples:
-   * APP::render('template');
-   * APP::render('template', $locals);
-   * APP::render('template', 'layout');
-   * APP::render('template', $locals, 'layout');
-   *
-   * @param string $name
-   * @param array $locals
-   * @param string $layout
-   */
-  public static function render ($name, $locals = array(), $layout = '') {
-    if (!is_array($locals)) {
-      $layout = $locals;
-      $locals = array();
+      $body = TEMPLATE::get($name, $locals);
+      if (empty($layout)) {
+        echo $body;
+      } else {
+        $locals['body'] = $body;
+        echo TEMPLATE::get($layout, $locals);
+      }
     }
 
-    foreach (APP::$locals as $i => $v) {
-      if (!isset($locals[$i])) $locals[$i] = $v;
+    /**
+     * 设置模板变量
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return mixed
+     */
+    public static function setLocals ($name, $value = null) {
+      TEMPLATE::$locals[$name] = $value;
+      return @TEMPLATE::$locals[$name];
     }
 
-    $body = APP::getTemplate($name, $locals);
-    if (empty($layout)) {
-      echo $body;
-    } else {
-      $locals['body'] = $body;
-      echo APP::getTemplate($layout, $locals);
-    }
-  }
-
-  public static function template ($name, $locals = array(), $layout = '') {
-    APP::render($name, $locals, $layout);
-  }
-
-  /**
-   * 设置模板变量
-   *
-   * @param string $name
-   * @param mixed $value
-   * @return mixed
-   */
-  public static function setLocals ($name, $value = null) {
-    APP::$locals[$name] = $value;
-    return @APP::$locals[$name];
-  }
-
-  /**
-   * 取模板变量
-   *
-   * @param string $name
-   * @return mixed
-   */
-  public static function getLocals ($name) {
-    return @APP::$locals[$name];
-  }
-
-  /**
-   * 返回JSON格式数据
-   *
-   * @param mixed $data
-   */
-  public static function sendJSON ($data = null) {
-    @header('content-type: application/json');
-    if (is_array($data) && APP::$is_debug) $data['debug'] = DEBUG::get();
-    echo json_encode($data);
-    APP::end();
-  }
-
-  /**
-   * 返回JSON格式的出错信息
-   *
-   * @param string $msg   出错信息
-   * @param array $data   其他数据
-   */
-  public static function sendError ($msg, $data = array()) {
-    $data['error'] = $msg;
-    APP::sendJSON($data);
-  }
-
-  /**
-   * 加载文件
-   * 文件名如果不指定扩展名，则自动加上.php再加载
-   * 如果以 / 开头，则从应用根目录开始查找
-   *
-   * @param string $filename
-   * @return mixed
-   */
-  public static function load ($filename) {
-    if (!pathinfo($filename, PATHINFO_EXTENSION)) {
-      $filename = $filename.'.php';
-    }
-    if (substr($filename, 0, 1) == '/') {
-      $filename = APP_ROOT.substr($filename, 1);
-    } else {
-      $filename = dirname($_SERVER["SCRIPT_FILENAME"]).'/'.$filename;
-    }
-    return require($filename);
-  }
-
-  /**
-   * 调试输出
-   *
-   * @param mixed $var
-   */
-  public static function dump ($var) {
-    echo '<pre>';
-    print_r($var);
-    echo '</pre>';
-  }
-
-  /**
-   * 初始化
-   */
-  public static function init () {
-    // 是否关闭出错显示
-    if (defined('APP_DEBUG') && APP_DEBUG) {
-      APP::$is_debug = true;
-      error_reporting(E_ALL);
-      ini_set('display_errors', '1');
-    } else {
-      error_reporting(0);
-      ini_set('display_errors', '0');
+    /**
+     * 取模板变量
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public static function getLocals ($name) {
+      return @TEMPLATE::$locals[$name];
     }
 
-    // 开始时间
-    define('APP_TIMESTAMP_START', microtime(true));
+  }
+}
 
-    // 只要定义了数据库配置中的任一项均自动连接数据库
-    if (defined('CONF_MYSQL_SERVER') || defined('CONF_MYSQL_USER') ||
-        defined('CONF_MYSQL_PASSWD') || defined('CONF_MYSQL_DBNAME')) {
-      $server = defined('CONF_MYSQL_SERVER') ? CONF_MYSQL_SERVER : 'localhost:3306';
-      $user = defined('CONF_MYSQL_USER') ? CONF_MYSQL_USER : 'root';
-      $passwd = defined('CONF_MYSQL_PASSWD') ? CONF_MYSQL_PASSWD : '';
-      $dbname = defined('CONF_MYSQL_DBNAME') ? CONF_MYSQL_DBNAME : '';
-      $permanent = defined('CONF_MYSQL_PERMANENT') ? CONF_MYSQL_PERMANENT : false;
-      SQL::connect($server, $user, $passwd, $dbname, $permanent);
-      if (defined('CONF_MYSQL_CHARSET')) SQL::charset(CONF_MYSQL_CHARSET);
+if (!class_exists('APP', false)) {
+  class APP {
+
+    // 版本号
+    public static $version = 1;
+
+    // 是否提前退出
+    public static $is_exit = false;
+
+    // 是否为调试状态
+    public static $is_debug = false;
+
+    /**
+     * 账户验证加密解密函数 （来自Discuz!的authcode函数）
+     *
+     * @param string $string 明文 或 密文
+     * @param string $operation DECODE表示解密,其它表示加密
+     * @param string $key 密匙
+     * @param int $expiry 密文有效期
+     *
+     * @return string
+     */
+    public static function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+      $ckey_length = 4;
+      $key = md5($key ? $key : 'leiphp-default-key');
+      $keya = md5(substr($key, 0, 16));
+      $keyb = md5(substr($key, 16, 16));
+      $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
+      $cryptkey = $keya.md5($keya.$keyc);
+      $key_length = strlen($cryptkey);
+      $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+      $string_length = strlen($string);
+      $result = '';
+      $box = range(0, 255);
+      $rndkey = array();
+      for($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+      }
+      for($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+      }
+      for($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+      }
+      if($operation == 'DECODE') {
+        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+          return substr($result, 26);
+        } else {
+          return '';
+        }
+      } else {
+        return $keyc.str_replace('=', '', base64_encode($result));
+      }
     }
 
-    // 自动执行 method_VERB
-    register_shutdown_function('_leiphp_request_method_router');
-  }
+    /**
+     * 加密账户验证信息
+     *
+     * @param string $string  要加密的字符串
+     * @param string $key     密匙
+     * @param int $expiry     从现在起的有效时间（秒）
+     * @return string
+     */
+    public static function authEncode ($string, $key, $expiry = 0) {
+      return APP::authcode($string, 'ENCODE', $key, $expiry);
+    }
 
-  /**
-   * 提前退出
-   */
-  public static function end () {
-    APP::$is_exit = true;
-    die;
+    /**
+     * 解密账户验证信息
+     *
+     * @param string $string  密文
+     * @param string $key     密匙
+     * @return string
+     */
+    public static function authDecode ($string, $key) {
+      return APP::authcode($string, 'DECODE', $key);
+    }
+
+    /**
+     * 加密密码
+     *
+     * @param string $password
+     * @return string
+     */
+    public static function encryptPassword ($password) {
+      $random = strtoupper(md5(rand().rand()));
+      $left = substr($random, 0, 2);
+      $right = substr($random, -2);
+      $newpassword = strtoupper(md5($left.$password.$right));
+      return $left.':'.$newpassword.':'.$right;
+    }
+
+    /**
+     * 验证密码
+     *
+     * @param string $password 待验证的密码
+     * @param string $encrypted 密码加密字符串
+     * @return bool
+     */
+    public static function validatePassword ($password, $encrypted) {
+      $random = explode(':', strtoupper($encrypted));
+      if (count($random) < 3) return false;
+      $left = $random[0];
+      $right = $random[2];
+      $main = $random[1];
+      $newpassword = strtoupper(md5($left.$password.$right));
+      return $newpassword == $main;
+    }
+
+    /**
+     * 显示出错信息
+     *
+     * @param string $msg
+     */
+    public static function showError ($msg) {
+      $accept_type = strtolower(trim($_SERVER['HTTP_ACCEPT']));
+      if (strpos($accept_type, 'json') !== false) {
+        APP::sendJSON(array('error' => $msg));
+      } else {
+        echo "<div style='color: #900;
+              font-size: 16px;
+              border: 1px solid #900;
+              padding: 8px 12px;
+              border-radius: 5px;
+              margin: 12px 0px;'>$msg</div>";
+      }
+    }
+
+    /**
+     * 返回JSON格式数据
+     *
+     * @param mixed $data
+     */
+    public static function sendJSON ($data = null) {
+      @header('content-type: application/json');
+      if (is_array($data) && APP::$is_debug) $data['debug'] = DEBUG::get();
+      echo json_encode($data);
+      APP::end();
+    }
+
+    /**
+     * 返回JSON格式的出错信息
+     *
+     * @param string $msg   出错信息
+     * @param array $data   其他数据
+     */
+    public static function sendError ($msg, $data = array()) {
+      $data['error'] = $msg;
+      APP::sendJSON($data);
+    }
+
+    /**
+     * 加载文件
+     * 文件名如果不指定扩展名，则自动加上.php再加载
+     * 如果以 / 开头，则从应用根目录开始查找
+     *
+     * @param string $filename
+     * @return mixed
+     */
+    public static function load ($filename) {
+      if (!pathinfo($filename, PATHINFO_EXTENSION)) {
+        $filename = $filename.'.php';
+      }
+      if (substr($filename, 0, 1) == '/') {
+        $filename = APP_ROOT.substr($filename, 1);
+      } else {
+        $filename = dirname($_SERVER["SCRIPT_FILENAME"]).'/'.$filename;
+      }
+      return require($filename);
+    }
+
+    /**
+     * 调试输出
+     *
+     * @param mixed $var
+     */
+    public static function dump ($var) {
+      echo "<pre style='font-size: 12px;
+      line-height: 1.6em;
+      text-align: left;
+      color: #02638e;
+      padding: 4px 20px;
+      border: 1px solid #ddd;
+      font-family: \"Microsoft yahei\", \"Helvetica Neue\", \"Lucida Grande\", \"Lucida Sans Unicode\", Helvetica, Arial, sans-serif !important;
+      background-color: #EEE;'>";
+      print_r($var);
+      echo '</pre>';
+    }
+
+    /**
+     * 初始化
+     */
+    public static function init () {
+      // 是否关闭出错显示
+      if (defined('APP_DEBUG') && APP_DEBUG) {
+        APP::$is_debug = true;
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+      } else {
+        error_reporting(0);
+        ini_set('display_errors', '0');
+      }
+
+      // 开始时间
+      define('APP_TIMESTAMP_START', microtime(true));
+
+      // 只要定义了数据库配置中的任一项均自动连接数据库
+      if (defined('CONF_MYSQL_SERVER') || defined('CONF_MYSQL_USER') ||
+          defined('CONF_MYSQL_PASSWD') || defined('CONF_MYSQL_DBNAME')) {
+        $server = defined('CONF_MYSQL_SERVER') ? CONF_MYSQL_SERVER : 'localhost:3306';
+        $user = defined('CONF_MYSQL_USER') ? CONF_MYSQL_USER : 'root';
+        $passwd = defined('CONF_MYSQL_PASSWD') ? CONF_MYSQL_PASSWD : '';
+        $dbname = defined('CONF_MYSQL_DBNAME') ? CONF_MYSQL_DBNAME : '';
+        $permanent = defined('CONF_MYSQL_PERMANENT') ? CONF_MYSQL_PERMANENT : false;
+        SQL::connect($server, $user, $passwd, $dbname, $permanent);
+        if (defined('CONF_MYSQL_CHARSET')) SQL::charset(CONF_MYSQL_CHARSET);
+      }
+
+      // 自动执行 method_VERB
+      register_shutdown_function('_leiphp_request_method_router');
+    }
+
+    /**
+     * 提前退出
+     */
+    public static function end () {
+      APP::$is_exit = true;
+      die;
+    }
   }
 }
